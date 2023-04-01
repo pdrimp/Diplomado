8e0266c16d0dd0056642ba527e92e0079c3f8fab
Pedro Damián Reyes <damian@ucol.mx>
2023-03-31 20:25:49 -0600

    Agregar archivos de proyecto.

---
+++ Diplomado/Server/Data/PersonasBDContext.cs
@@ -0,0 +1,15 @@
+﻿using Diplomado.Shared.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace Diplomado.Server.Data
+{
+	public class PersonasBDContext : DbContext
+	{
+		public PersonasBDContext(DbContextOptions<PersonasBDContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Persona> Personas { get; set; }
+	}
+}
---
+++ Diplomado/Shared/Modelos/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Diplomado.Shared.Modelos
+{
+	public class Persona
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "El nombre es obligatorio")]
+		[StringLength(100, ErrorMessage = "Tamaño máximo 100 caracteres")]
+		public string? Nombre { get; set; }
+		[Required(ErrorMessage = "El correo es obligatorio")]
+		[StringLength(100, ErrorMessage = "Tamaño máximo 100 caracteres")]
+		[EmailAddress(ErrorMessage = "Escribir un correo válido")]
+		public string? Correo { get; set; }
+		[Required(ErrorMessage = "El teléfono es obligatorio")]
+		[StringLength(10, ErrorMessage = "Tamaño máximo 10 caracteres")]
+		[Phone(ErrorMessage = "Escribir un teléfono válido")]
+		public string? Telefono { get; set; }
+	}
+}
